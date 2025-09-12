78023f625ff30fe94d5a4c894b59a4dd72a9b818
Sherlockcmd <921277132@qq.com>
2025-09-12 18:38:32 +0800

    JavaStudy

---
+++ JavaWeb/src/com/easy/web/pojo/Area.java
@@ -0,0 +1,38 @@
+package com.easy.web.pojo;
+
+public class Area {
+    private int id;
+    private String name;
+
+    public Area() {
+    }
+
+    public Area(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Area{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
